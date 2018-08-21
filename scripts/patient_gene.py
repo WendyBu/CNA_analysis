7e0370fb2cdfa08ce44406bb4a7f2a6eb21cec87
yiwenbu <wybyw@hotmail.com>
2018-08-21 18:20:02 -0500

    patient vs chrom_genes heatmap

--- scripts/patient_gene.py
+++ scripts/patient_gene.py
@@ -10,7 +10,11 @@ import glob
 import os
 import os.path
 
-
+def order_table(df, chr):
+    chr_df = pd.read_csv("../chrom_gene_list/"+chr+"_gene.csv", sep="\t", index_col=4)
+    merged_df = chr_df.join(df, how="right")
+    merged_df.sort_values("start", inplace=True)
+    return merged_df
 
 
 def generate_heatmap_table(keyword="Melanoma", chr="chr8"):
@@ -18,16 +22,18 @@ def generate_heatmap_table(keyword="Melanoma", chr="chr8"):
     input_path = os.path.join("..", keyword+"_temp", chr, "part_CNA")
     for file in glob.glob(input_path+"/*"):
         df_cna = pd.read_csv(file, sep="\t",index_col=0)
+        if "Cytoband" in df_cna.columns:
+            df_cna.drop("Cytoband", axis=1, inplace=True)
         if not main_df.empty:
             main_df = main_df.join(df_cna, lsuffix='', rsuffix='_y')
         else:
             main_df = df_cna
         # print main_df.shape
-    main_df =  main_df.replace(r'\s+', np.nan, regex=True)
-    main_df.fillna(0, inplace=True)
-    output_path = os.path.join("..", keyword+"_temp", chr, "heatmap_"+keyword+"_"+chr+".xls")
-    main_df.to_csv(output_path, sep="\t")
-
+    df_ordered = order_table(main_df, chr)
+    df_ordered = df_ordered.replace(r'\s+', np.nan, regex=True)
+    df_ordered.fillna(0, inplace=True)
+    output_dir = os.path.join("..", keyword+"_temp", "reports", "heatmap_"+keyword+"_"+chr+".xls")
+    df_ordered.to_csv(output_dir,sep="\t")
 
 
 def main(keyword="Melanoma"):
@@ -38,7 +44,8 @@ def main(keyword="Melanoma"):
         generate_heatmap_table(keyword, chr)
     pass
 
-
+keywordlist = ["Prostate","Breast", "Bladder", "Colon", "GBM"]
 if __name__ == "__main__":
-    main()
+    for key in keywordlist:
+        main(key)
 
